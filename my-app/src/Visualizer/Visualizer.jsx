7415bb27d88e43d476074d462e2aa8d0eb25a010
ken-10 <46728332+ken-10@users.noreply.github.com>
2020-06-04 22:58:51 -1000

    implemented a more robust testing method

--- my-app/src/Visualizer/Visualizer.jsx
+++ my-app/src/Visualizer/Visualizer.jsx
@@ -32,6 +32,19 @@ export class Visualizer extends React.Component {
         console.log(equalArrays(jsSortedArray, mergeSortedArray));
     }
 
+    testSortAlgorithm() {
+        for (let i = 0; i < 100; i++) {
+            const array = [];
+            const length = randomInteger(1, 1000);
+            for (let i = 0; i < length; i++) {
+                array.push(randomInteger(-1000, 1000));
+            }
+            const jsSortedArray = this.state.array.slice().sort((a,b) => a - b);
+            const mergeSortedArray = Algorithms.mergeSort(this.state.array);
+            console.log(equalArrays(jsSortedArray, mergeSortedArray));
+        }
+    }
+
     render() {
         const {array} = this.state;
     
@@ -44,6 +57,7 @@ export class Visualizer extends React.Component {
                 <button onClick={() => this.resetArray()}>Generate New Array</button>
                 <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
                 <button onClick={() => this.mergeSort()}>Merge Sort</button>
+                <button onClick={() => this.testSortAlgorithm()}>Test Sorting Algorithm</button>
 
             </div>
         );
@@ -53,7 +67,7 @@ export class Visualizer extends React.Component {
 function randomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
+//to check if sorting algorithms actually sort correctly by comparing it to an array sorted by sort function
 function equalArrays(array1, array2) {
     if (array1.length !== array2.length) return false;
     for (var i = 0; i < array1.length; i++) {
